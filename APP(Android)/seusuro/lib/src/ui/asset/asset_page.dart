ef982cc144f48602990c956b15b1f39c8a326221
Axolotl <dorcus501@gmail.com>
2022-10-02 11:25:24 +0000

    PropertyPage 재산 총 개수 text 추가

--- APP(Android)/seusuro/lib/src/ui/asset/asset_page.dart
+++ APP(Android)/seusuro/lib/src/ui/asset/asset_page.dart
@@ -3,10 +3,42 @@ import 'package:flutter/material.dart';
 class AssetPage extends StatelessWidget {
   const AssetPage({super.key});
 
+  final _valueList = ['가나다 순', '최신 등록 순', '유효기간 짧은 순'];
+  var _selectedValue = '가나다순';
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('재산 현황'),
-    );
+    return const Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  color: AppColors().textBlack,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+              ),
+              children: [
+                TextSpan(
+                  text: '총 '
+                ),
+                TextSpan(
+                  text: '$totalNum',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400
+                  )
+                ),
+                TextSpan(
+                  text:'개'
+                ),
+              ]
+            ),
+            
+          ],
+          mainAxisAlignment: MainAxisAlignment.spaceBetween
+        )
+      ],
+    )
   }
 }
