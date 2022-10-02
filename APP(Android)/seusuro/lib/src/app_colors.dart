1929977e6370506429b2aaf54d907acca31d40fb
MedtoPublic <54583524+MedtoPublic@users.noreply.github.com>
2022-10-02 10:22:45 +0000

    AppColors 수정

--- APP(Android)/seusuro/lib/src/app_colors.dart
+++ APP(Android)/seusuro/lib/src/app_colors.dart
@@ -4,14 +4,25 @@ class AppColors {
   // Key
   Color keyRed = const Color(0xFFED8BC0);
   Color keyBlue = const Color(0xFF8BA1ED);
+  Color keyGreen = const Color(0xFF6DDB8C);
+  Color keyGrey = const Color(0xFF808080);
 
   // Background
   Color bgWhite = const Color(0xFFFFFFFF);
+  Color bgRed = const Color(0xFFFF0000);
+  Color bgLightRed = const Color(0xFFFF5050);
+  Color bgGreen = const Color(0xFF04B900);
+  Color bgYellow = const Color(0xFFE8D000);
 
   // Text
   Color textBlack = const Color(0xFF000000);
-  Color textGrey = const Color(0xFFD9D9D9);
   Color textWhite = const Color(0xFFFFFFFF);
+  Color textGrey = const Color(0xFF808080);
+  Color textBlue = const Color(0xFF808080);
+  Color textGreen = const Color(0xFF04B900);
+  Color textPurple = const Color(0xFFAD00FF);
+  Color textOrange = const Color(0xFFFF7A00);
+  Color textBrown = const Color(0xFFCD6B10);
 
   // Line
   Color lineGrey = const Color(0xFFD9D9D9);
