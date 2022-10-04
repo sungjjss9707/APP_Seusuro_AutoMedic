a6865d942d820fed79afc5d434c07566d453ce05
Axolotl <dorcus501@gmail.com>
2022-10-04 13:48:58 +0000

    IndividualItem 클래스에서 메서드로 변경 및 추가

--- APP(Android)/seusuro/lib/src/ui/asset/individual_item.dart
+++ APP(Android)/seusuro/lib/src/ui/asset/individual_item.dart
@@ -1,15 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:seusuro/src/app_colors.dart';
 
-class IndividualItem extends StatelessWidget {
-  const IndividualItem({super.key});
-  
-  @override
-  Widget build(BuildContext context) {
-    return Row(
+Widget individualItem(String category, String productName, int ea) {
+  return Container(
+    padding: const EdgeInsets.all(16.0),
+    alignment: Alignment.center,
+    child: Row(
       children: [
-        
-      ],
-    );
-  }
-  
+        const SizedBox(
+          height: 64,
+          width: 64
+        ),
+        const SizedBox(width: 16),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(category),
+            Text(
+              productName,
+              style: const TextStyle(fontWeight: FontWeight.w700)
+            ),
+            RichText(
+              text: TextSpan(
+                children: [
+                  const TextSpan(text: '유효기간'),
+                  WidgetSpan(
+                    child: Icon(Icons.circle,
+                      size: 12,
+                      color: AppColors().statusGreen
+                    )
+                  )
+                ]
+              )
+            )
+          ]
+        ),
+        const SizedBox(width: 44),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              '총 보유량',
+              style: TextStyle(fontWeight: FontWeight.w700),
+            ),
+            Text(
+              '$ea EA',
+              style: const TextStyle(fontWeight: FontWeight.w700),
+            )
+          ],
+        ),
+        const SizedBox(width: 32),
+        const Icon(
+          Icons.arrow_forward,
+          size: 24
+        )
+      ]
+    )
+  );
 }
