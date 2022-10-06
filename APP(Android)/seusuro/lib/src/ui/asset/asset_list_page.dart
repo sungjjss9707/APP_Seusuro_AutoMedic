2dc9916f6a9e516de607bbf2de4c66a4728ca4c1
Axolotl <dorcus501@gmail.com>
2022-10-06 11:13:40 +0000

    AssetListPage 클래스 더미데이터 추가

--- APP(Android)/seusuro/lib/src/ui/asset/asset_list_page.dart
+++ APP(Android)/seusuro/lib/src/ui/asset/asset_list_page.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
 import 'package:seusuro/src/ui/asset/individual_item.dart';
+import 'package:seusuro/src/ui/asset/asset.dart';
 
 class AssetList extends StatefulWidget {
   const AssetList({super.key});
@@ -14,6 +16,56 @@ class AssetList extends StatefulWidget {
 class _AssetListState extends State<AssetList> {
   List<dynamic> assets = [];
   
+  static DateFormat dateFormat = DateFormat("yyyy년 MM월 dd일");
+
+  List<dynamic> testAssets = [
+    Asset(
+      id: '1234567890',
+      name: '아세트아미노펜',
+      unit: 'EA',
+      totalAmount: 2500,
+      amountByPlace: [
+
+      ],
+      expirationDate: dateFormat.parse("2022년 10월 27일"),
+      logRecord: [
+
+      ],
+      createdAt: dateFormat.parse('2022년 09월 12일'),
+      updatedAt: dateFormat.parse('2022년 10월 06일')
+    ),
+    Asset(
+      id: '1234567890',
+      name: '아세트아미노펜',
+      unit: 'EA',
+      totalAmount: 2500,
+      amountByPlace: [
+
+      ],
+      expirationDate: dateFormat.parse("2022년 10월 27일"),
+      logRecord: [
+
+      ],
+      createdAt: dateFormat.parse('2022년 09월 12일'),
+      updatedAt: dateFormat.parse('2022년 10월 06일')
+    ),
+    Asset(
+      id: '1234567890',
+      name: '아세트아미노펜',
+      unit: 'EA',
+      totalAmount: 2500,
+      amountByPlace: [
+
+      ],
+      expirationDate: dateFormat.parse("2022년 10월 27일"),
+      logRecord: [
+
+      ],
+      createdAt: dateFormat.parse('2022년 09월 12일'),
+      updatedAt: dateFormat.parse('2022년 10월 06일')
+    ),
+  ];
+
   @override
   void initState() {
     super.initState();
@@ -39,14 +91,38 @@ class _AssetListState extends State<AssetList> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: assets.length,
-      itemBuilder: (BuildContext context, int index) {
-        return individualItem(assets[index]['category'], assets[index]['name'], assets[index]['totalAmount']);
-      },
-      separatorBuilder: (BuildContext context, int index) {
-        return const Divider();
-      },
-    );
+    if (testAssets.isNotEmpty) {
+      return Container(
+        width: 360,
+        height: 579,
+        child: ListView.separated(
+          itemCount: testAssets.length,
+          itemBuilder: (context, index) {
+            return individualItem('경구약', testAssets[index].name, testAssets[index].totalAmount);
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return const Divider();
+          },
+        ),
+      );
+    } else {
+      return Container(
+        width: 360,
+        height: 579,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            SizedBox(height: 265,),
+            Text(
+              '재산이 없습니다.',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+              ),
+            )
+          ],
+        )
+      );
+    }
   }
 }
