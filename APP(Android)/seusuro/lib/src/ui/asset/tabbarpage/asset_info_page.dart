cee5f86b7672d42952d2197e056225a31a147a02
Axolotl <dorcus501@gmail.com>
2022-10-10 14:14:37 +0000

    AssetInfoPage 보관장소별 수량 차트 추가

--- APP(Android)/seusuro/lib/src/ui/asset/tabbarpage/asset_info_page.dart
+++ APP(Android)/seusuro/lib/src/ui/asset/tabbarpage/asset_info_page.dart
@@ -2,85 +2,110 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:seusuro/src/app_colors.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:time_machine/time_machine.dart';
 
-Widget assetInfoPage(Map<String, dynamic> json) {
-  String expirationDate = DateFormat('yyyy년 MM월 dd일').format(json['expirationDate']); 
+Widget assetInfoPage(Map<String, dynamic> assetInfo) {
+  String expirationDate = DateFormat('yyyy년 MM월 dd일').format(assetInfo['expirationDate']); 
 
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Row(
-        children: [
-          const Text('기본 정보', style: TextStyle(fontWeight: FontWeight.w900)),
-          const SizedBox(width: 8),
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.info_outline_rounded,
-                size: 16,
-                color: AppColors().textBlack,
-              ))
-        ],
-      ),
-      Column(
-        children: [
-          Row(
-            children: [
-              SizedBox(
-                width: 64,
-                child: Text(
-                  '분류',
-                  style: TextStyle(color: AppColors().textGrey),
-                )
-              ),
-              Text(json['category']),
-            ],
-          ),
-          Row(
-            children: [
-              SizedBox(
-                width: 64,
-                child: Text(
-                  'NIIN',
-                  style: TextStyle(color: AppColors().textGrey),
-                )
-              ),
-              Text(json['niin']),
-            ],
-          ),
-          Row(
-            children: [
-              SizedBox(
-                width: 64,
-                child: Text(
-                  '총량',
-                  style: TextStyle(color: AppColors().textGrey),
-                )
-              ),
-              Text('${json['totalAmount']} ${'unit'}')
-            ],
-          ),
-          Row(
-            children: [
-              SizedBox(
-                width: 64,
-                child: Text(
-                  '유효기간',
-                  style: TextStyle(color: AppColors().textGrey),
-                )
-              ),
-              Text(expirationDate),
-            ],
-          ),
-        ],
-      ),
-      const Text(
-        '장소별 보유량',
-        style: TextStyle(fontWeight: FontWeight.w900)
-      ),
-      barChart(json['amountByPlace']),
-    ],
+  return Container(
+    margin: const EdgeInsets.all(32.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            const Text('기본 정보', style: TextStyle(fontWeight: FontWeight.w900)),
+            const SizedBox(width: 4),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.info_outline_rounded,
+                  size: 16,
+                  color: AppColors().textBlack,
+                ))
+          ],
+        ),
+        Column(
+          children: [
+            Row(
+              children: [
+                SizedBox(
+                  width: 64,
+                  child: Text(
+                    '분류',
+                    style: TextStyle(color: AppColors().textGrey),
+                  )
+                ),
+                Text(assetInfo['category']),
+              ],
+            ),
+            const SizedBox(height: 16,),
+            Row(
+              children: [
+                SizedBox(
+                  width: 64,
+                  child: Text(
+                    'NIIN',
+                    style: TextStyle(color: AppColors().textGrey),
+                  )
+                ),
+                Text(assetInfo['niin']),
+              ],
+            ),
+            const SizedBox(height: 16,),
+            Row(
+              children: [
+                SizedBox(
+                  width: 64,
+                  child: Text(
+                    '총량',
+                    style: TextStyle(color: AppColors().textGrey),
+                  )
+                ),
+                Text('${assetInfo['totalAmount']} ${assetInfo['unit']}')
+              ],
+            ),
+            const SizedBox(height: 16,),
+            Row(
+              children: [
+                SizedBox(
+                  width: 64,
+                  child: Text(
+                    '유효기간',
+                    style: TextStyle(color: AppColors().textGrey),
+                  )
+                ),
+                const SizedBox(width: 8),
+                Text(expirationDate),
+                warningSign(assetInfo['expirationDate']),
+              ],
+            ),
+          ],
+        ),
+        const SizedBox(height: 32),
+        const Text(
+          '장소별 보유량',
+          style: TextStyle(fontWeight: FontWeight.w900)
+        ),
+        barChart(assetInfo['amountByPlace']),
+      ],
+    ),
   );
+}
+
+Widget warningSign(DateTime expirationDate) {
+  LocalDate expDate = LocalDate.dateTime(expirationDate);
+  LocalDate curr = LocalDate.today();
+  Period diff = expDate.periodSince(curr);
+  if (diff.years == 0 && diff.months < 6) {
+    return Icon(
+      Icons.warning_amber_rounded,
+      color: AppColors().statusRed,
+      size: 20,
+    );
+  } else {
+    return Container();
+  }
 }
 
 SfCartesianChart barChart(List<Map<String, dynamic>> amountByPlace) {
@@ -96,7 +121,13 @@ SfCartesianChart barChart(List<Map<String, dynamic>> amountByPlace) {
       ColumnSeries<ChartData, String> (
         dataSource: chartData,
         xValueMapper: (ChartData data, _) => data.storagePlace,
-        yValueMapper: (ChartData data, _) => data.quantity
+        yValueMapper: (ChartData data, _) => data.quantity,
+        dataLabelSettings: const DataLabelSettings(
+          labelAlignment: ChartDataLabelAlignment.top,
+          isVisible: true,
+        ),
+        width: 0.4,
+        color: AppColors().chartGrey,
       )
     ]
   );
