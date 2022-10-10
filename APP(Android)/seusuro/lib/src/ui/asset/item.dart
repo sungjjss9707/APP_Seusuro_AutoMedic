de77b6430d22d2ff1da012743dbaafe9eb081461
Axolotl <dorcus501@gmail.com>
2022-10-10 14:14:21 +0000

    Item onPressed 구현

--- APP(Android)/seusuro/lib/src/ui/asset/item.dart
+++ APP(Android)/seusuro/lib/src/ui/asset/item.dart
@@ -1,10 +1,13 @@
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:seusuro/src/app_colors.dart';
+import 'package:seusuro/src/ui/asset/tab_page.dart';
+import 'package:time_machine/time_machine.dart';
 
-Widget item(String category, String name, String unit, int ea) {
+Widget item(Map<String, dynamic> assetInfo) {
   return TextButton(
     onPressed: () {
-      
+      Get.to(() => TabPage(assetInfo));
     },
     style: TextButton.styleFrom(
       padding: EdgeInsets.zero
@@ -30,36 +33,37 @@ Widget item(String category, String name, String unit, int ea) {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  category,
+                  assetInfo['category'],
                   style: TextStyle(
                     fontWeight: FontWeight.w700,
-                    color: textColor(category),
+                    fontSize: 12,
+                    color: textColor(assetInfo['category']),
                   ),
                 ),
                 Text(
-                  name,
+                  assetInfo['name'],
                   style: TextStyle(
                     fontWeight: FontWeight.w700,
+                    fontSize: 16,
                     color: AppColors().textBlack,
                   ),
                 ),
-                RichText(
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: '유효기간 ',
-                        style: TextStyle(
-                          color: AppColors().textBlack,
-                        )
-                      ),
-                      WidgetSpan(
-                        child: Icon(Icons.circle,
-                          size: 12,
-                          color: AppColors().statusGreen
-                        )
+                Row(
+                  children: [
+                    Text(
+                      '유효기간 ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 12,
+                        color: AppColors().textGrey,
                       )
-                    ]
-                  )
+                    ),
+                    Icon(
+                      Icons.circle,
+                      size: 12,
+                      color: statusColor(assetInfo['expirationDate']),
+                    ),
+                  ],
                 )
               ]
             ),
@@ -75,7 +79,7 @@ Widget item(String category, String name, String unit, int ea) {
                 ),
               ),
               Text(
-                '${ea}${unit}',
+                '${assetInfo['totalAmount']}${assetInfo['unit']}',
                 style: TextStyle(
                   fontWeight: FontWeight.w700,
                   color: AppColors().textBlack,
@@ -93,6 +97,21 @@ Widget item(String category, String name, String unit, int ea) {
       ),
     ),
   );
+}
+
+Color statusColor(DateTime expirationDate) {
+  LocalDate expDate = LocalDate.dateTime(expirationDate);
+  LocalDate curr = LocalDate.today();
+  Period diff = expDate.periodSince(curr);
+  if (diff.years == 0 && diff.months < 6) {
+    return AppColors().statusRed;
+  } else if (diff.years == 0 && diff.months >= 6) {
+    return AppColors().statusYellow;
+  } else if (diff.years >= 1) {
+    return AppColors().statusGreen;
+  } else {
+    return AppColors().bgWhite;
+  }
 }
 
 Color textColor(String category) {
