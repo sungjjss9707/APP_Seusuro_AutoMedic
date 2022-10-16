8f957e0d458c47c43b3d022b090ad1e14975f5b0
Axolotl <dorcus501@gmail.com>
2022-10-16 10:47:43 +0000

    PropertyRepository class 추가

---
+++ APP(Android)/seusuro/lib/src/repository/property_repository.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:seusuro/src/model/dto/register_request_dto.dart';
+import 'package:seusuro/src/model/dto/response_dto.dart';
+import 'package:seusuro/src/repository/base_url.dart';
+
+import 'package:http/http.dart' as http;
+
+class PropertyRepository {
+  Future<ResponseDto> viewIndividualProperty(String inherentID) async {
+    var url = Uri.parse('$baseUrl/property/$inherentID');
+
+    http.Response response = await http.get(url);
+
+    return ResponseDto.fromJson(jsonDecode(response.body));
+  }
+
+  Future<ResponseDto> viewBookmarkedProperty() async {
+    var url = Uri.parse('$baseUrl/property/favorite');
+
+    http.Response response = await http.get(url);
+
+    return ResponseDto.fromJson(jsonDecode(response.body));
+  }
+
+  Future<ResponseDto> addBookmarkedProperty(String inherentID) async {
+    var url = Uri.parse('$baseUrl/property/favorite');
+
+    http.Response response = await http.post(
+      url,
+      body: {
+        'id': inherentID
+      },
+    );
+
+    return ResponseDto.fromJson(jsonDecode(response.body));
+  }
+}
