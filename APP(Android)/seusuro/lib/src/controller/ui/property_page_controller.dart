878e463b18af4cb62507be814022b88271165fd6
Axolotl <dorcus501@gmail.com>
2022-10-16 10:47:22 +0000

    PropertyPageController 분류 항목 bool 값들 추가

--- APP(Android)/seusuro/lib/src/controller/ui/property_page_controller.dart
+++ APP(Android)/seusuro/lib/src/controller/ui/property_page_controller.dart
@@ -1,12 +1,19 @@
 import 'package:get/get.dart';
 
-class AssetPageController extends GetxController {
-  static AssetPageController get to => Get.find();
+class PropertyPageController extends GetxController {
+  static PropertyPageController get to => Get.find();
 
   RxInt orderIndex = 0.obs;
   RxInt sortIndex = 0.obs;
   RxInt dateIndex = 0.obs;
   RxInt locIndex = 0.obs;
+
+  RxBool includeOrally = false.obs;
+  RxBool includeVaccine = false.obs;
+  RxBool includeAerosol = false.obs;
+  RxBool includeGuard = false.obs;
+  RxBool includeMask = false.obs;
+  RxBool includeConsumable = false.obs;
 
   void changeOrderIndex(int index) {
     orderIndex(index);
