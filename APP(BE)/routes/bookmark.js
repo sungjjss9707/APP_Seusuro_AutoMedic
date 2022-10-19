49d5c6231f3a782be4990931e0b55970e59c6d8b
성준혁 <sungjjss9707>
2022-10-19 15:44:12 +0000

    북마크 추가 작업

--- APP(BE)/routes/bookmark.js
+++ APP(BE)/routes/bookmark.js
@@ -163,6 +163,16 @@ router.post('/', async function(req, res, next) {
     }
     var militaryUnit = check_militaryUnit_result[0].militaryUnit;
 	var medicine_name = req.body.id;
+	var entpName = req.body.entpName;
+	var efcyQesitm = req.body.efcyQesitm;
+	var useMethodQesitm = req.body.useMethodQesitm;
+	var atpnWarnQesitm = req.body.atpnWarnQesitm;
+	var atpnQesitm = req.body.atpnQesitm;
+	var intrcQesitm = req.body.intrcQesitm;
+	var seQesitm = req.body.intrcQesitm;
+	var depositMethodQesitm = req.body.depositMethodQesitm;
+	var itemImage = req.body.itemImage;
+
 	var insert_bookmark_sql = "insert into bookmark_"+militaryUnit+" values (?, ?);";
 	var insert_bookmark_param = [user_id, medicine_name];
 	var insert_bookmark_success = await myQuery(insert_bookmark_sql, insert_bookmark_param);
@@ -179,9 +189,33 @@ router.post('/', async function(req, res, next) {
 		await con.rollback();
         return;
 	}
+	var select_medicine_sql = "select * from medicine where itemName = ?;";
+	var select_medicine_param = medicine_name;
+	var [select_medicine_result] = await con.query(select_medicine_sql, select_medicine_param);
+	if(select_medicine_result.length==0){
+		var insert_medicine_sql = "insert into medicine values (?,?,?,?,?,?,?,?,?,?);";
+		var insert_medicine_param = [entpName, medicine_name, efcyQesitm, useMethodQesitm, atpnWarnQesitm, atpnQesitm, intrcQesitm, seQesitm, depositMethodQesitm, itemImage];
+		var insert_medicine_result = await myQuery(insert_medicine_sql, insert_medicine_param);
+		if(!insert_medicine_result){
+			res.send({status:500, message:"Internal Server Error", data:null});
+        	await con.rollback();
+        	return;
+		}
+	}
+
 	var data = [];
+	var individual_data;
 	for(let i=0; i<select_bookmark_result.length; ++i){
-		data.push(select_bookmark_result[i].name);
+		select_medicine_sql = "select * from medicine where itemName = ?;";
+		select_medicine_param = select_bookmark_result[i].name;
+		var [result] = await con.query(select_medicine_sql, select_medicine_param);
+		if(result.length==0){
+			res.send({status:500, message:"Internal Server Error", data:null});
+        	await con.rollback();
+        	return;
+		}
+		individual_data = {entpName:result[0].entpName, itemName:result[0].itemName, efcyQesitm:result[0].efcyQesitm, useMethodQesitm:result[0].useMethodQesitm, atpnWarnQesitm:result[0].atpnWarnQesitm, atpnQesitm:result[0].atpnQesitm, intrcQesitm:result[0].intrcQesitm, seQesitm:result[0].seQesitm, depositMethodQesitm:result[0].depositMethodQesitm, itemImage:result[0].itemImage };
+		data.push(individual_data);
 	}
 	res.send({status:200, message:"Ok", data:data});
 	await con.commit();
